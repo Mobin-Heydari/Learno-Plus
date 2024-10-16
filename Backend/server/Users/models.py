62c65b65cb70fd2f81ab8a6788abb832c8ab5ace
Mobin_Developer <special.me.force1@gmail.com>
2024-10-16 12:46:46 +0330

    User models and tabels added to database and project.

--- Backend/server/Users/models.py
+++ Backend/server/Users/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    
+    class UserTypes(models.Choices):
+        TEACHER = "Teacher"
+        STUDENT = "Student"
+
+        
+    user_type = models.CharField(
+        max_length=7, 
+        choices=UserTypes.choices,
+    )
+    
+    phone = models.CharField(
+        max_length=11,
+        unique=True
+    )
+    
+    username = models.CharField(
+        max_length=40,
+        unique=True
+    )
+    
+    email = models.EmailField(unique=True)   
+    
+    joined_date = models.DateField(auto_now_add=True)
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    
+    
+    USERNAME_FIELD = "phone"
+    REQUIRED_FIELDS = ["email", "username"]
+
+    
+    class Meta:
+        ordering = ['joined_date']
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    
+    def __str__(self):
+        return self.username
+    
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
