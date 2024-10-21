b0cc5e490fe1ad19ec1d41d08751c169950cc6f8
Mobin_Developer <special.me.force1@gmail.com>
2024-10-21 17:03:26 +0330

    Users Profile model is add to the Profiles app.

--- Backend/server/Profiles/models.py
+++ Backend/server/Profiles/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from Users.models  import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE, 
+        related_name="user_profile"
+    )
+    
+    bio = models.TextField(
+        max_length=500, 
+        blank=True,
+        null=True
+    )
+    
+    image = models.ImageField(
+        upload_to='profiles/',
+        blank=True,
+        null=True
+    )
+
+    full_name = models.CharField(
+        max_length=200,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return f"{self.user.username}.....{self.full_name}"
+
+
+    class Meta:
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles" 
