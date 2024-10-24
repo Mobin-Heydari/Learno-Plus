cb99c53ea4320e7503b25479a158d13b4852cd5a
Mobin_Developer <special.me.force1@gmail.com>
2024-10-24 13:21:44 +0330

    Jwt settings is added to the settings of the project.

--- Backend/server/server/settings.py
+++ Backend/server/server/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +42,8 @@ INSTALLED_APPS = [
     
     # Third Party Apps
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
+    'corsheaders',
     
     # Custom Apps
     'Users.apps.UsersConfig',
@@ -55,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'server.urls'
@@ -124,6 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -133,3 +141,63 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Auth user model
 AUTH_USER_MODEL = 'Users.User'
+
+# Rest framework
+REST_FRAMEWORK = {
+    #  Authentications classes
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
+# JWT settings
+SIMPLE_JWT = {
+    #  Tokens life time
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=50),
+    
+    #  Refresh tokens
+    'ROTATE_REFRESH_TOKENS': True,
+    
+    # Blacklist 
+    'BLACKLIST_AFTER_ROTATION': True,
+    
+    # Last Login refreshing
+    'UPDATE_LAST_LOGIN': True,
+    
+    # Algorithm
+    'ALGORITHM': 'HS256',
+    
+    # Verifying key 
+    'VERIFYING_KEY': None,
+    
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+    
+    # Headers 
+    'AUTH_HEADER_TYPES': ('Bearer',),     # Header type
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',   # Header name
+    
+    # User id setting
+    'USER_ID_FIELD': 'id',         # Field
+    'USER_ID_CLAIM': 'user_id',    # Claim
+    
+    #  Auth rules settings
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+    
+    # Auth token classes and settings
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+    'JTI_CLAIM': 'jti',
+    
+    #  Sliding settings
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    # Token sliding lifetime
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
+
+CORS_ALLOW_ALL_ORIGINS = True
