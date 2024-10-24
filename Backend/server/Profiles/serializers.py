def18c9c656859502d95478b01dc146f20655b56
Mobin_Developer <special.me.force1@gmail.com>
2024-10-24 11:57:36 +0330

    UserProfileSerializer is optimized.

--- Backend/server/Profiles/serializers.py
+++ Backend/server/Profiles/serializers.py
@@ -2,10 +2,21 @@ from rest_framework import serializers
 
 from .models import UserProfile
 
+from Users.serializers import UserSerializer
+
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
     
+    user = serializers.SerializerMethodField()
+    
     class Meta:
         model = UserProfile
-        fields = "__all__"
+        fields = ['id', 'bio', 'full_name', 'image', 'user']
+        
+        
+    def get_user(self, obj):
+        
+        serializer = UserSerializer(instance=obj.user)
+        
+        return serializer.data
